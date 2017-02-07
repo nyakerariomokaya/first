fb097eb29764e2f358d1c8a64ac2d57f1fbe4f6d
nyakerariomokaya <mokayanyakerario@gmail.com>
2017-02-07 04:01:19 +0300

    Second commit

---
+++ prime_numbers.py
@@ -0,0 +1,13 @@
+def is_prime():
+    n = int(raw_input("Find the primes upto?:"))
+    
+    for num in range(2,n+1):
+	is_prime = True
+	for x in range(2,num):
+	    if num % x  == 0:
+	       is_prime = False
+	       break
+        if is_prime == True:
+           print(num)
+    
+is_prime() 
